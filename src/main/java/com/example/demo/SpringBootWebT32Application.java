edc9e8413652efe88b70bec5a388f90c82276eeb
Can <614237899@qq.com>
2018-10-17 10:44:10 +0800

    123

---
+++ src/main/java/com/example/demo/SpringBootWebT32Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebT32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebT32Application.class, args);
+	}
+}
